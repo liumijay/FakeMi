999871dc6de5f900b2efcfa70fe6342c21e90535
LMJ <1245832008@qq.com>
2020-02-24 14:58:08 +0800

    ssh密钥没加==!

---
+++ app/src/main/java/com/lmj/fakemi/type/pic/PicFragment.java
@@ -0,0 +1,9 @@
+package com.lmj.fakemi.type.pic;
+
+/*
+ * Author:        LMJ
+ * CreateDate:     2020/2/24 14:57
+ * Description:     图片列表
+ */
+public class PicFragment {
+}
---
+++ app/src/main/java/com/lmj/fakemi/type/pic/PicViewModel.java
@@ -0,0 +1,9 @@
+package com.lmj.fakemi.type.pic;
+
+/*
+ * Author:        LMJ
+ * CreateDate:     2020/2/24 14:57
+ * Description:     作用描述
+ */
+public class PicViewModel {
+}
